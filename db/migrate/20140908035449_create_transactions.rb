180d5c2259ce6f14f6edf83c8eed0e6dc1bb021c
LegitMiller <jordan.kay@gmail.com>
2015-05-16 21:29:34 -0600

    test text for database

--- db/migrate/20140908035449_create_transactions.rb
+++ db/migrate/20140908035449_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
       t.integer :cash
-      t.string :name
+      t.text :name
       t.integer :envelope_id
 
       t.timestamps
